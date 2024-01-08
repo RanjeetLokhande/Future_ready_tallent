8b32c684d28bba5deaf8d458ee1f79c186b5d015
RanjeetLokhande <104196701+RanjeetLokhande@users.noreply.github.com>
2024-01-08 14:31:38 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,60 +1,55 @@
 (() => {
-    const form = document.querySelector("form")
-    const select = document.querySelector("select")
-    const org_text = document.querySelector("input")
-    let result = document.querySelector("#result")
+    const form = document.querySelector("form");
+    const select = document.querySelector("#language");
+    const org_text = document.querySelector("#orgText");
+    let result = document.querySelector("#result");
 
     const fetchResult = () => {
-        fetch("/all").then(res => res.json()).then(data => {
-            console.log(data)
-            renderResult(data)
-        })
-    }
-
-    fetchResult()
+        fetch("/all")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                renderResult(data);
+            })
+            .catch(error => console.error(error));
+    };
 
     const renderResult = (data) => {
-        data.forEach( ({orgText, transText, langCode}) => {
-            let li = document.createElement("li")
-            li.innerText = `${orgText} - ${transText} - ${langCode}`
-
-            result.appendChild(li)
+        data.forEach(({ orgText, transText, langCode }) => {
+            let li = document.createElement("li");
+            li.innerText = `${orgText} - ${transText} - ${langCode}`;
+            result.appendChild(li);
         });
-    }
+    };
 
     form.addEventListener("submit", e => {
-        try {
-            e.preventDefault()
+        e.preventDefault();
 
-            console.log(org_text)
-
-            fetch(`/translate`,{
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    orgText: org_text.value,
-                    langCode: select.value
-                })
+        fetch("/translate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                orgText: org_text.value,
+                langCode: select.value
             })
-                .then(res => res.json())
-                .then(data => { 
-                    if(!data) {
-                        console.log("Something went wrong!")
-                        return
-                    }
-                    
-                    let tran_text = data.result[0].translations[0].text
-                    
-                    console.log("Translated Text : ", tran_text)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (!data) {
+                    console.log("Something went wrong!");
+                    return;
+                }
 
-                    window.location.reload()
-                }, console.error)
-        } catch (e) {
-            console.log("Error While  submiting the form : ", e)
-        }
+                let tran_text = data.result[0].translations[0].text;
 
-    })
+                console.log("Translated Text : ", tran_text);
 
-})()
+                window.location.reload();
+            })
+            .catch(error => console.error(error));
+    });
+
+    fetchResult();
+})();
